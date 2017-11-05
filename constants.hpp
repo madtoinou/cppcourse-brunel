9dbfd3cfd12b9a9ef110ae8db5e5f38f9debc217
madtoinou <antoine.madrona@epfl.ch>
2017-11-05 16:44:20 +0100

    put h instead of 0.1 in the decleration of the variable n

--- constants.hpp
+++ constants.hpp
@@ -8,6 +8,6 @@ int const g (-5); ///< Relative strength of inhibitory synapses (J_I =J*g)
 int const eta (2); ///< 
 double const Nu_threshold (10); ///<
 double const Nu_ext (eta*Nu_threshold); ///< number of spikes/second needed to reach threshold
-double const n (Nu_ext*0.1); ///< the frequency of external input (spikes/step)
+double const n (Nu_ext*h); ///< the frequency of external input (spikes/step)
 
 #endif
